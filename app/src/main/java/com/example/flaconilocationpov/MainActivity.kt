16c89f83c58bf0aad8787a7f076966746438e461
Bassem <BasemOmar2012@gmail.com>
2023-12-13 11:37:55 +0000

    requesting permissions by ui

--- app/src/main/java/com/example/flaconilocationpov/MainActivity.kt
+++ app/src/main/java/com/example/flaconilocationpov/MainActivity.kt
@@ -11,6 +11,7 @@ import android.util.Log
 import com.example.flaconilocationpov.databinding.ActivityMainBinding
 import android.Manifest
 import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.ActivityCompat
@@ -48,28 +49,60 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        requestPerms()
-        createNotificationChannel()
+
+        permissionCheck()
         val intentFilter = IntentFilter(AppConstants.DISTANCE_BROADCAST).also {
             it.addAction(AppConstants.LOCATION_BROADCAST)
         }
         registerReceiver(broadcastReceiver, intentFilter)
         binding.confirmHome.setOnClickListener {
             currentLocation?.let { it1 -> storeLocation(it1) }
-        //    throw RuntimeException("Test Crash") // Force a crash
+            //    throw RuntimeException("Test Crash") // Force a crash
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        createNotificationChannel()
+    }
+
+    private fun permissionCheck() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPerms()
+
+        } else {
+            createNotificationChannel()
+
         }
     }
 
     private fun requestPerms() {
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.ACCESS_COARSE_LOCATION,
-                Manifest.permission.ACCESS_NOTIFICATION_POLICY
-            ),
-            100
-        )
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ),
+                100
+            )
+        }
     }
 
     private fun createNotificationChannel() {
